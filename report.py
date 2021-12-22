75f9606fe6e95e70d3d79a4866b2f7f1855371f8
Nidhin Joseph <nidhinjos@gmail.com>
2021-12-22 11:03:56 +0000

    added ping

--- report.py
+++ report.py
@@ -9,6 +9,8 @@ def access_report_gen():
     
     ftp_report = pd.read_csv('ftp_report.csv')
     ps_report = pd.read_csv('port_scan_report.csv')
+    pc_report = pd.read_csv('ping_report.csv')
+    
 
     # 2. Create a template Environment
     env = Environment(loader=FileSystemLoader('templates'))
@@ -24,7 +26,11 @@ def access_report_gen():
                            ftp_access_text='FTP Access Report',
                            ftp_access_report=ftp_report,
                            port_scan_text='Port Scan Report',
-                           port_scan_report=ps_report)
+                           port_scan_report=ps_report,
+                           ping_check_text='Ping Report',
+                           ping_check_report=pc_report)
    #5. Write the template to an HTML file
     with open('honeypot_access_report.html', 'w') as f:
         f.write(html)
+
+    print("honeypot_access_report.html generated and is available in the root folder")
